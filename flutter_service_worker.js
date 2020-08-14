7b92ee8c65c8a06276e8488184d1aa551dbce94b
Yazid-Krayem <ykrayyem@gmail.com>
2020-08-14 12:42:46 +0300

    Built web
    Branch: master
    Commit: f1fdf85dd53cf24631c952d0e82b4129ca8302ed
    
    package:peanut 3.5.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "a7a0119dbd13b1446ad536458dd6763d",
 "/": "a7a0119dbd13b1446ad536458dd6763d",
-"main.dart.js": "f7738d05e5c1ecea4370da28433d649c",
+"main.dart.js": "6ae13060e6f047e2af93f70820a5cee7",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
