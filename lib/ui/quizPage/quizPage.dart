dc9986e99de510a4f10d5f9c219d6340850efb6a
Yazid-Krayem <ykrayyem@gmail.com>
2020-08-19 13:49:11 +0300

    images

--- lib/ui/quizPage/quizPage.dart
+++ lib/ui/quizPage/quizPage.dart
@@ -338,25 +338,33 @@ class _QuizPageState extends State<QuizPage> with TickerProviderStateMixin {
   Widget indexStacked() {
     var orientation = MediaQuery.of(context).orientation;
 
-    return Container(
-      alignment: Alignment.center,
-      width: orientation == Orientation.portrait
-          ? MediaQuery.of(context).size.width * 0.6
-          : MediaQuery.of(context).size.width * 0.45,
-      height: MediaQuery.of(context).size.height * 0.3,
-      child: IndexedStack(
-          index: currentIndex,
-          children: questionsListTest.map((question) {
-            if (questionsListTest.indexOf(question) <= 3) {
-              return QuestionsList(
-                  currentIndex: currentIndex,
-                  progress: _progress,
-                  question: question);
-            } else {
-              return Container();
-            }
-          }).toList()),
-    );
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: Container(
+          alignment: Alignment.center,
+          width: orientation == Orientation.portrait
+              ? MediaQuery.of(context).size.width * 0.6
+              : MediaQuery.of(context).size.width * 0.45,
+          height: MediaQuery.of(context).size.height * 0.3,
+          decoration: BoxDecoration(
+            color: Colors.lightBlueAccent,
+            gradient: LinearGradient(
+              colors: [Colors.white, Colors.blue],
+            ),
+          ),
+          child: IndexedStack(
+              index: currentIndex,
+              children: questionsListTest.map((question) {
+                if (questionsListTest.indexOf(question) <= 3) {
+                  return QuestionsList(
+                      currentIndex: currentIndex,
+                      progress: _progress,
+                      question: question);
+                } else {
+                  return Container();
+                }
+              }).toList()),
+        ));
   }
 
   @override
